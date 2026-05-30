923b86d3f129fc9cb5fbf3260596cd950750fd9a
Jean Lucas <jean.l.silva@edu.senai.br>
2026-05-30 17:01:06 +0000

    Lição 8 - Tarefa 1: Melhorar a HelpCommandprodução

--- command_runner/lib/src/help_command.dart
+++ command_runner/lib/src/help_command.dart
@@ -2,15 +2,11 @@ import 'dart:async';
 
 import 'arguments.dart';
 
-// Prints program and argument usage.
-//
-// When given a command as an argument, it prints the usage of
-// that command only, including its options and other details.
-// When the flag 'verbose' is set, it prints options and details for all commands.
-//
-// This command isn't automatically added to CommandRunner instances.
-// Packages users should add it themselves with [CommandRunner.addCommand],
-// or create their own command that prints usage.
+import 'package:command_runner/command_runner.dart';
+
+import 'console.dart';
+
+import 'exceptions.dart';
 
 class HelpCommand extends Command {
   HelpCommand() {
@@ -19,6 +15,7 @@ class HelpCommand extends Command {
       abbr: 'v',
       help: 'When true, this command will print each command and its options.',
     );
+
     addOption(
       'command',
       abbr: 'c',
@@ -26,6 +23,7 @@ class HelpCommand extends Command {
           "When a command is passed as an argument, prints only that command's verbose usage.",
     );
   }
+
   @override
   String get name => 'help';
 
@@ -36,12 +34,60 @@ class HelpCommand extends Command {
   String? get help => 'Prints this usage information';
 
   @override
-  FutureOr<Object?> run(ArgResults args) async {
-    var usage = runner.usage;
-    for (var command in runner.commands) {
-      usage += '\n ${command.usage}';
+  FutureOr<String> run(ArgResults args) async {
+    final buffer = StringBuffer();
+    buffer.writeln(runner.usage.titleText);
+
+    if (args.flag('verbose')) {
+      for (var cmd in runner.commands) {
+        buffer.write(_renderCommandVerbose(cmd));
+      }
+
+      return buffer.toString();
     }
 
-    return usage;
+    if (args.hasOption('command')) {
+      var (:option, :input) = args.getOption('command');
+
+      var cmd = runner.commands.firstWhere(
+        (command) => command.name == input,
+        orElse: () {
+          throw ArgumentException(
+            'Input ${args.commandArg} is not a known command.',
+          );
+        },
+      );
+
+      return _renderCommandVerbose(cmd);
+    }
+
+    // Verbose is false and no arg was passed in, so print basic usage.
+    for (var command in runner.commands) {
+      buffer.writeln(command.usage);
+    }
+
+    return buffer.toString();
+  }
+
+  String _renderCommandVerbose(Command cmd) {
+    final indent = ' ' * 10;
+    final buffer = StringBuffer();
+
+    buffer.writeln(cmd.usage.instructionText);
+    buffer.writeln('$indent ${cmd.help}');
+
+    if (cmd.valueHelp != null) {
+      buffer.writeln(
+        '$indent [Argument] Required? ${cmd.requiresArgument}, Type: ${cmd.valueHelp}, Default: ${cmd.defaultValue ?? 'none'}',
+      );
+    }
+
+    buffer.writeln('$indent Options:');
+
+    for (var option in cmd.options) {
+      buffer.writeln('$indent ${option.usage}');
+    }
+
+    return buffer.toString();
   }
 }
