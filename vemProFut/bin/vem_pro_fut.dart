576952d8fc876944f4fa048ea967b14ff0e3ed4f
Gabriel Barbosa <gabriel.b.silva11@edu.senai.br>
2026-06-21 00:49:09 +0000

    Desafio: O Simulador da Fase de Grupos – Copa 2026 - Projeto 'vemProFut'

---
+++ vemProFut/bin/vem_pro_fut.dart
@@ -0,0 +1,5 @@
+import 'package:vem_pro_fut/vem_pro_fut.dart' as vem_pro_fut;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vem_pro_fut.calculate()}!');
+}
---
+++ vemProFut/test/vem_pro_fut_test.dart
@@ -0,0 +1,8 @@
+import 'package:vem_pro_fut/vem_pro_fut.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
